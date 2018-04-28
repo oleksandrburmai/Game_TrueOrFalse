d256df66de6ac3efa3e157129f6c78602bc5d057
oleksandrburmai <alex.burmai@yahoo.com>
2018-04-28 21:02:33 +0300

    create user registration and authentication with email verification

--- app/src/main/java/com/alex/game/LoginActivity.java
+++ app/src/main/java/com/alex/game/LoginActivity.java
@@ -1,27 +1,113 @@
 package com.alex.game;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListner;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        mEmailField = findViewById(R.id.l_email);
+        mPasswordField = findViewById(R.id.l_password);
+
+        findViewById(R.id.btn_login).setOnClickListener(this);
+        findViewById(R.id.btn_register).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthStateListner = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                } else {
+
+                }
+            }
+        };
     }
 
-    public void RegisterOnClick(View view) {
-        Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 
-    public void LoginOnClick(View view) {
-        Intent intent = new Intent(LoginActivity.this,MainMenuActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
+    public void signIn(String email, String password) {
+        if (!validateForm()) {
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful() && Objects.requireNonNull(mAuth.getCurrentUser()).isEmailVerified()) {
+                            Toast.makeText(LoginActivity.this, "Авторизація успішна",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainMenuActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
+                        } else
+                            Toast.makeText(LoginActivity.this, "Введенно не вірний email або пароль",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_login) {
+            signIn(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        } else if (v.getId() == R.id.btn_register) {
+            Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Введіть email");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Введіть пароль");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+        return valid;
     }
 }
--- app/src/main/java/com/alex/game/RegisterActivity.java
+++ app/src/main/java/com/alex/game/RegisterActivity.java
@@ -1,13 +1,90 @@
 package com.alex.game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListner;
+
+    private EditText mEmailField;
+    private EditText mPasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
+        mEmailField = findViewById(R.id.r_email);
+        mPasswordField = findViewById(R.id.r_password);
+
+        findViewById(R.id.btn_Rregister).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void registration(String email, String password) {
+        if (!validateForm()) {
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email, password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Objects.requireNonNull(mAuth.getCurrentUser()).sendEmailVerification();
+                            Toast.makeText(RegisterActivity.this, "Регістрація успішна",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                            startActivity(intent);
+
+                        } else
+                            Toast.makeText(RegisterActivity.this, "Введенно не вірний email",
+                                    Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_Rregister) {
+            registration(mEmailField.getText().toString(), mPasswordField.getText().toString());
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = mEmailField.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            mEmailField.setError("Введіть email");
+            valid = false;
+        } else {
+            mEmailField.setError(null);
+        }
+
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            mPasswordField.setError("Введіть пароль");
+            valid = false;
+        } else {
+            mPasswordField.setError(null);
+        }
+        return valid;
     }
 }
